2e7a0bb4934889fb5d66c8ef4b151bf9cc74ec4a
Joost Steltenpool <jcsteltenpool@users.noreply.github.com>
2022-12-20 09:52:32 +0100

    Updates 1

--- main.js
+++ main.js
@@ -1,116 +1,149 @@
 import { getSolution } from "./modules/words.js";
 
-/* RENDER GAME FIELD */
-let solution = getSolution();
-console.log('Solution: ' + solution);
+window.addEventListener('DOMContentLoaded', () => {
 
-renderGameField(6, solution.length);
 
-function renderGameField(rows, columns) {
-    const container = document.createElement('section');
-    container.setAttribute('class', 'gameContainer');
-    
-    for (let i = 0; i < rows; i++) {
-        let row = document.createElement('div');
-        row.setAttribute('class', 'row');
-        container.appendChild(row);
-        for (let i = 0; i < columns; i++) {
-                    let rowCell = document.createElement('div');
-                    rowCell.setAttribute('class', 'rowCell');
-                    row.appendChild(rowCell);
-            }
-    }
-    document.querySelector('main').prepend(container);
-};
+    /* RENDER GAME FIELD */
+    let solution = getSolution();
+    // let solution = 'WATER';
+    console.log('Solution: ' + solution);
 
-/* KEYBOARD */
+    renderGameContainer(solution.length);
 
-const keys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
-const backspace = document.getElementById('backspace');
-const enter = document.getElementById('enter');
-
-const inputs_array = [];
-keys.forEach((key) => {
-    inputs_array.push(document.getElementById(key));
-})
-
-/* PLAY GAME */
-
-function playGame() {
-    let r = 0;
-    let c = 0;
-    let targetCell = document.getElementsByClassName('row')[r].children[c];
-    let guess = [];
-    
-    for(let i = 0; i < inputs_array.length; i++) {
-        let inputLetter = inputs_array[i].innerHTML;
-        inputs_array[i].addEventListener('click', function() {
-            targetCell.innerHTML = inputLetter;
-            if (c <= solution.length) {
-                targetCell = document.getElementsByClassName('row')[r].children[c += 1];
-            }
-            guess.push(inputLetter);
-        });
-    }
-
-    backspace.addEventListener('click', function() {
-        if (c > 0) {
-            targetCell = document.getElementsByClassName('row')[r].children[c -= 1];
-            targetCell.innerHTML = '';
-            guess.pop();
-            // console.log(guess);     
+    function renderGameContainer(columns) {
+        const container = document.createElement('section');
+        container.setAttribute('id', 'gameContainer');
+        
+        for (let i = 0; i < 6; i++) {
+            let row = document.createElement('div');
+            row.setAttribute('class', 'row');
+            container.appendChild(row);
+            for (let i = 0; i < columns; i++) {
+                        let rowCell = document.createElement('div');
+                        rowCell.setAttribute('class', 'rowCell');
+                        row.appendChild(rowCell);
+                }
         }
-    });
+        document.querySelector('main').prepend(container);
+    };
 
-    enter.addEventListener('click', function() {
-        if (r < 6 && c === solution.length) {
-            guess = guess.join('');
-            // console.log(guess);
-            let result = compare(guess, solution);
-            if (guess === solution) {
-                setTimeout(() => {
-                    alert('Win!')
-                }, 500);;
-            }
-            console.log('Result: ' + result);
-            targetCell = document.getElementsByClassName('row')[r += 1].children[c = 0];
-            guess = [];
-        } else if (r != solution.length) {
-            alert('Het ingevoerde woord is te kort.')
-        }
+    /* KEYBOARD */
+
+    const keys = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+    const backspace = document.getElementById('backspace');
+    const enter = document.getElementById('enter');
+
+    const inputs_array = [];
+    keys.forEach((key) => {
+        inputs_array.push(document.getElementById(key));
     })
 
-    function compare(guess, solution) {
-        for (let i = 0; i < solution.length; i++) {
-          let guessLetter = guess.charAt(i);
-          console.log(guessLetter);
-          let solutionLetter = solution.charAt(i);
-          targetCell = document.getElementsByClassName('row')[r].children[i];
-          let guessKey = document.getElementById(guessLetter);
+    /* PLAY GAME */
 
-          if (guessLetter === solutionLetter) {
-            targetCell.style.backgroundColor = 'green';
-            targetCell.style.border = '2px solid green';
-            targetCell.style.color = 'white';
-            guessKey.style.backgroundColor = 'green';
-          }
-          else if (solution.indexOf(guessLetter) != -1) {
-            targetCell.style.backgroundColor = '#ffdd00';
-            targetCell.style.border = '2px solid #ffdd00';
-            if (guessKey.style.backgroundColor != 'green') {
-                guessKey.style.backgroundColor = '#ffdd00';
-            }
-          }
-          else {
-            targetCell.style.backgroundColor = 'darkgray';
-            targetCell.style.border = '2px solid darkgray';
-            guessKey.style.backgroundColor = 'darkgray';
-          }
+    function playGame() {
+        let r = 0;
+        let c = 0;
+        let targetCell = document.getElementsByClassName('row')[r].children[c];
+        let guess = [];
+
+        let winMessage = document.getElementById('winMessage');
+        let loseMessage = document.getElementById('loseMessage');
+        let showSolution = document.getElementsByClassName('solution');
+        for (let i = 0; i < showSolution.length; i++) {
+            showSolution[i].innerHTML = solution;
         }
-      }
-}
 
-playGame();
+        for(let i = 0; i < inputs_array.length; i++) {
+            let inputLetter = inputs_array[i].innerHTML;
+            inputs_array[i].addEventListener('click', function() {
+                targetCell.innerHTML = inputLetter;
+                if (c <= solution.length) {
+                    targetCell = document.getElementsByClassName('row')[r].children[c += 1];
+                }
+                guess.push(inputLetter);
+            });
+        }
 
-// console.log(inputs_array);
-// inputs_array[1].style.backgroundColor = 'green';
+        backspace.addEventListener('click', function() {
+            if (c > 0) {
+                targetCell = document.getElementsByClassName('row')[r].children[c -= 1];
+                targetCell.innerHTML = '';
+                guess.pop();
+            }
+        });
+
+        enter.addEventListener('click', checkGuess);
+        
+        function checkGuess() {
+            if (c === solution.length) {
+                let gameContainer = document.getElementById('gameContainer');
+                guess = guess.join('');
+                compare(guess, solution);
+                if (guess === solution) {
+                    setTimeout(() => {
+                        winMessage.style.display = 'block';
+                        gameContainer.style.visibility = 'hidden';
+                    }, 500);
+                    enter.removeEventListener('click', checkGuess);
+                }
+                else if (r === 5 && c === solution.length && guess !== solution) {
+                    loseMessage.style.display = 'block';
+                    gameContainer.style.visibility = 'hidden';
+                }
+                targetCell = document.getElementsByClassName('row')[r += 1].children[c = 0];
+                guess = [];
+            } else if (c !== solution.length) {
+                alert('Het ingevoerde woord is te kort.')
+            }
+        }
+
+        function compare(guess, solution) {
+            let misplaced = [];
+            let solutionTemp = solution.split("");
+            
+            for (let i = 0; i < solution.length; i++) {
+                let guessLetter = guess.charAt(i);
+                let solutionLetter = solution.charAt(i);
+                let guessKey = document.getElementById(guessLetter);
+                targetCell = document.getElementsByClassName('row')[r].children[i];
+                
+                function alterSolutionTemp() {
+                    delete solutionTemp[solution.indexOf(guessLetter)];
+                }
+
+                if (guessLetter === solutionLetter) {
+                    alterSolutionTemp();
+                    console.log(solutionTemp);
+                    
+                    targetCell.style.backgroundColor = 'green';
+                    targetCell.style.border = '2px solid green';
+                    targetCell.style.color = 'white';
+                    guessKey.style.backgroundColor = 'green';
+                }
+                else if (solution.indexOf(guessLetter) != -1) {
+                    if (solutionTemp.includes(guessLetter)) {
+                        misplaced.push(guessLetter);
+                    }
+                    targetCell.style.backgroundColor = '#ffdd00';
+                    targetCell.style.border = '2px solid #ffdd00';
+                    if (guessKey.style.backgroundColor != 'green') {
+                        guessKey.style.backgroundColor = '#ffdd00';
+                    }
+                }
+                else {
+                    targetCell.style.backgroundColor = 'darkgray';
+                    targetCell.style.border = '2px solid darkgray';
+                    guessKey.style.backgroundColor = 'darkgray';
+                }
+            }
+            // for (let i = 0; i < misplaced.length; i++) {
+            //     console.log(misplaced[i]);
+            // }
+            console.log('Misplaced: ' + misplaced);
+
+        }
+    }
+
+    playGame();
+
+});
